ccef6c367dddfe95cc34ec12470d2cc507c4d807
jivan1399 <jivanbansod1@gmail.com>
2022-11-14 13:29:01 +0530

    BreakCheck Commit

---
+++ src/main/java/com/gitTest/GitBranchTestApplication.java
@@ -0,0 +1,13 @@
+package com.gitTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBranchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBranchTestApplication.class, args);
+	}
+
+}
